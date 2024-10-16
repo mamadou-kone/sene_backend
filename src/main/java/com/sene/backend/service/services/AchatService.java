1cd0d3e4aa4ba7543d02bc27999668e398184e97
mamadou-kone <momedkone2@gmail.com>
2024-10-16 09:58:12 +0000

    modification de achat 1

--- src/main/java/com/sene/backend/service/services/AchatService.java
+++ src/main/java/com/sene/backend/service/services/AchatService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 @Service
 public class AchatService implements CrudService<Achat, Long> {
 
+    @Autowired // Ajoutez cette annotation pour injecter le repository
     private AchatRepository achatRepository;
 
     @Override
